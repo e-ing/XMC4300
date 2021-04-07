1fdffbdc28e977eea4de62e2bbd8c75d14475668
e-ing <dmitriy.i-sys@yandex.ru>
2021-04-07 13:07:59 +0700

    Keil project created

---
+++ RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,27 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ob' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "XMC4300.h"
+
+/*  Infineon::Device:XMClib:GPIO:2.1.24 */
+#define RTE_DEVICE_XMCLIB_GPIO
+/*  Infineon::Device:XMClib:SPI:2.1.24 */
+#define RTE_DEVICE_XMCLIB_SPI
+/*  Infineon::Device:XMClib:UART:2.1.24 */
+#define RTE_DEVICE_XMCLIB_UART
+
+
+#endif /* RTE_COMPONENTS_H */
